55475fc5ff9d1957153da8d5b2103819b5cb2bdf
vishwajeet-4847 <exclusivebloh@gmail.com>
2024-09-22 15:26:53 +0530

    portfolio backend 2

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const SendMail = require('./mail.js');
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap;"); 
-    next();
+  res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com;");
+  next();
 });
+
 
 app.post('/submit-form', (req, res) => {
   const { name, email, phone, message } = req.body;
